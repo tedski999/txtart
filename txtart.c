413d587218cda60bf18a36bb185aee1297414b07
tedski999 <tedjohnsonjs@gmail.com>
2022-01-10 23:21:05 +0000

    Add command-line args parser
    Currently accepts --help, --version and --load.

--- txtart.c
+++ txtart.c
@@ -1,8 +1,33 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <ncurses.h>
 
 int main(int argc, char **argv) {
+
+	char *active_file = NULL;
+	for (int i = 1; i < argc; i++) {
+		char *arg = argv[i];
+		if (!strcmp(arg, "-h") || !strcmp(arg, "--help") || !strcmp(arg, "-?")) {
+			// TODO: print help
+			printf("txtart %s - Simple ncurses ASCII art editor\n", TXTART_VERSION);
+			return EXIT_SUCCESS;
+		} else if (!strcmp(arg, "-v") || !strcmp(arg, "--version")) {
+			printf("txtart %s\n", TXTART_VERSION);
+			return EXIT_SUCCESS;
+		} else if (!strcmp(arg, "-l") || !strcmp(arg, "--load")) {
+			if (++i >= argc) {
+				fprintf(stderr, "%s requires an argument.\n", arg);
+				return EXIT_FAILURE;
+			}
+			active_file = argv[i];
+		} else {
+			fprintf(stderr, "Unknown argument '%s'.\n", arg);
+			return EXIT_FAILURE;
+		}
+	}
+
 	initscr();
 	cbreak();
 	noecho();
